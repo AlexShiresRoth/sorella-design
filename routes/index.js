ac0ea252c7ebc54cb5f46243c7496c038bff88aa
Alex Roth <alexroth96@gmail.com>
2019-03-16 20:10:58 +0000

    add basic styling and structure to file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Sorella Design' });
+exports.index = (req, res) => {
+  res.render('index', { 
+      title: 'Sorella Design', 
+      slogans: ['color planning', 'surface design', 'interiors'], 
+      about: {
+          title: 'About Sorella Design',
+          content: [`lorem ipsum dolor amet, lorem ipsum dolor amet
+          lorem ipsum dolor amet lorem ipsum dolor amet lorem ipsum dolor amet
+          lorem ipsum dolor amet lorem ipsum dolor amet lorem ipsum dolor amet
+          lorem ipsum dolor amet lorem ipsum dolor amet lorem ipsum dolor amet 
+          lorem ipsum dolor amet lorem ipsum dolor amet lorem ipsum dolor amet 
+          lorem ipsum dolor amet lorem ipsum dolor amet lorem ipsum dolor amet`]
+      }
+  });
 };
